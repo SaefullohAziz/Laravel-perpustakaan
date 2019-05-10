98d1801e7bb7f9e43665986cd901a4777431783a
Saefulloh Aziz <43395311+SaefullohAziz@users.noreply.github.com>
2019-05-10 13:04:10 +0700

    try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Perpustakaan
                 </div>
 
                 <div class="links">
